efb0c36362d4d53e3963c0eb267f3a15dcff04c8
KOUAME <ksergeromeo@gmail.com>
2023-03-01 10:51:02 +0000

    Sauvegarde 1

---
+++ resources/views/entetes/navbar.blade.php
@@ -0,0 +1,6 @@
+<ul>
+    <li>Accueil</li>
+    <li>Client</li>
+    <li>Employé</li>
+    <li>Opération</li>
+</ul>
---
+++ resources/views/layouts/employe.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+<h1>Page employé</h1>
+@endsection
